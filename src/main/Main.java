362a155314341326a10634a4e5dbb751cba591f9
Mael <mael.ogier.38@gmail.com>
2014-12-07 20:51:36 +0100

    add GEXF generation example

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,10 @@ package main;
 
 import java.util.Vector;
 
-import services.SpotightConnection;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.TestGenerator;
 
+import model.GEXFStaticGraphExample;
+import services.SpotightConnection;
 public class Main {
 
 	/**
@@ -12,10 +14,16 @@ public class Main {
 	 */
 	public static void main(String[] args) throws Exception {
 		
+		//Example DBPedia Spotlight
 		SpotightConnection spCon = new SpotightConnection();
-		
-		Vector<String> termsNotSpotlighted = spCon.sendGETRequest("motor");
+		Vector<String> termsNotSpotlighted = spCon.sendGETRequest("Renault motor");
 		System.out.println(termsNotSpotlighted.toString());
+		
+		//Example GEXF file generation
+		GEXFStaticGraphExample graphExample = new GEXFStaticGraphExample();
+		graphExample.generateExample();
+		
+		
 	}
 
 }
